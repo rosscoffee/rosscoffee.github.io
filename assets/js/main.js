39b2d8c9700d78f42cd709c29f00d83c653c9376
Bubble <halobubblevy@gmail.com>
2023-12-21 07:38:08 +0700

    Update Layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,17 @@
-let bar = document.querySelector(".bar");
-let nav = document.querySelector(".navigation");
-let body = document.querySelector("body");
-
-// Fungsi untuk menutup navigasi jika diklik di luar area sidebar
-function closeNavOutsideClick(event) {
-    if (event.target !== bar && event.target.closest('.navigation') === null) {
-        body.style.overflow = '';
-        nav.style.left = "-100%";
-        bar.src = "assets/images/others/menu.png";
-        document.removeEventListener('click', closeNavOutsideClick);
+function myNav(){
+    let bar = document.querySelector(".bar");
+    let nav = document.querySelector(".navigation");
+    bar.onclick = ()=>{
+        if(nav.style.left == "0%"){
+            nav.style.left = "-100%";
+            bar.src = "assets/images/others/menu.png"
+        }else{
+            nav.style.left = "0%";
+            bar.src = "assets/images/others/x.png"
+        }
     }
 }
-
-bar.onclick = () => {
-    if (nav.style.left === "0%") {
-        body.style.overflow = '';
-        nav.style.left = "-100%";
-        bar.src = "assets/images/others/menu.png";
-    } else {
-        body.style.overflow = 'hidden';
-        nav.style.left = "0%";
-        bar.src = "assets/images/others/x.png";
-        // Menambahkan event listener untuk menutup navigasi saat klik di luar area sidebar
-        document.addEventListener('click', closeNavOutsideClick);
-    }
-};
-
+myNav();
 
 window.onload = ()=>{
     let homeContent = document.querySelector(".home-content");
